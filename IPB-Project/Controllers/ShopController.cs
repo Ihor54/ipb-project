f3830146710396145212d7f8279b36dd4cb8e4a2
ihor_debelyi <s15293@pjwstk.edu.pl>
2019-06-15 11:06:55 +0200

    project is done

---
+++ IPB-Project/Controllers/ShopController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IPB_Project.Models;
+
+namespace IPB_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            ViewBag.HeaderNeeded = false;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.HeaderNeeded = true;
+            ViewBag.Header = "Contact";
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            ViewBag.HeaderNeeded = true;
+            ViewBag.Header = "About Us";
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            ViewBag.HeaderNeeded = true;
+            ViewBag.Header = "Product";
+            return View();
+        }
+      public IActionResult Services()
+        {
+            ViewBag.HeaderNeeded = true;
+            ViewBag.Header = "Services";
+            return View();
+        }
+
+      public IActionResult Privacy()
+      {
+          ViewBag.HeaderNeeded = false;
+            return View();
+      }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
